9d642718242f68b2d567b5f6dd45be0d855dfac5
Bibek <bibekacharya788@gmailcom>
2025-06-18 16:48:06 +0545

    cs50 function and variable completed

---
+++ function and variables/calculator.py
@@ -0,0 +1,5 @@
+x=int(input("enter value of x:"))
+y=int(input("enter value of y:"))
+
+print(x+y)
+print(f"{1000:,}")
---
+++ user defined function/functionBasics.py
@@ -0,0 +1,6 @@
+name=input("enter your name")
+def hello(name="world"):
+    print(f"hello {name}")
+hello(name)    
+
+ 
