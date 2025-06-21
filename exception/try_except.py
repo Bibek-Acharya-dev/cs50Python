32c78e1d89669e61df3ddb889882c05aa7e85b71
Bibek <bibekacharya788@gmailcom>
2025-06-21 15:21:55 +0545

    handled exception

--- exception/try_except.py
+++ exception/try_except.py
@@ -14,12 +14,22 @@
 
 # another approach
 
+# while True:
+#     try:
+#         x = int(input("What's x? "))
+#         print("x is positive" if x >= 0 else "x is negative")
+#         break  # break only if input is valid
+#     except ValueError:
+#         print("x is not an integer")
+
+
+
+#    pass
+
 while True:
     try:
         x = int(input("What's x? "))
         print("x is positive" if x >= 0 else "x is negative")
         break  # break only if input is valid
     except ValueError:
-        print("x is not an integer")
-
-    
+        pass
